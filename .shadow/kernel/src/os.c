21886afa3567c9c7e004ae11bce8296b9be2323b
tracer-nju <tracer@nju.edu.cn>
2025-05-12 20:55:37 +0800

    Linux wniux 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:55:37 up  2:25, 11 users,  load average: 0.82, 1.42, 1.74

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -18,7 +18,9 @@ static void os_run()
         putch(*s == '*' ? '0' + cpu_current() : *s);
     }
     yield();
-    while (1);
+    while (1){
+        printf("Hello World from CPU %d\n", cpu_current());
+    }
 }
 static void os_on_irq(int seq, int event, handler_t handler)
 {

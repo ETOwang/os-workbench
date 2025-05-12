65159418012785b584497526ddf10ea6c865cafc
tracer-nju <tracer@nju.edu.cn>
2025-05-12 20:57:10 +0800

    Linux wniux 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:57:10 up  2:27, 11 users,  load average: 0.75, 1.25, 1.65

--- .shadow/kernel/src/dev/dev.c
+++ .shadow/kernel/src/dev/dev.c
@@ -39,7 +39,8 @@ static void dev_init() {
   devices[id]->ops->init(devices[id]);
 
   DEVICES(INIT);
-
+  
+  printf("init %s\n", devices[0]->name);
   kmt->create(pmm->alloc(sizeof(task_t)), "input-task", dev_input_task, NULL);
   kmt->create(pmm->alloc(sizeof(task_t)), "tty-task",   dev_tty_task,   NULL);
 }

1fc96c6ff1b1f8c2602ec1b39261d81f417fb022
tracer-nju <tracer@nju.edu.cn>
2025-05-19 14:08:59 +0800

    Linux wniux 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:08:59 up 9 min,  1 user,  load average: 1.47, 1.37, 0.74

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -293,7 +293,7 @@ static void kmt_spin_unlock(spinlock_t *lk)
         for (int i = 0; i <MAX_TASK; i++)
         {
             if(tasks[i]!=NULL&&tasks[i]->cpu!=-1){
-               printf("task %s is not dead\n",tasks[i]->name);
+               printf("task %s is %d\n",tasks[i]->name,tasks[i]->status);
                printf("task %s is in cpu %d\n",tasks[i]->name,tasks[i]->cpu);
             }
         }

e322b8a747dee990ebcdaa8c5d972f9d0ecc6310
tracer-nju <tracer@nju.edu.cn>
2025-05-19 11:05:54 +0800

    Linux wniux 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:05:54 up 54 min,  1 user,  load average: 4.36, 1.98, 1.48

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -27,10 +27,10 @@ static void os_init()
     pmm->init();
     kmt->spin_init(&handler_lock, "handler_lock");
     kmt->init();
-    //dev->init();
-    kmt->sem_init(&empty, "empty", 10);
+    dev->init();
+    kmt->sem_init(&empty, "empty", 10000);
     kmt->sem_init(&full,  "fill",  0);
-    for (int i = 0; i <1; i++) {
+    for (int i = 0; i <32; i++) {
         kmt->create(pmm->alloc(sizeof(task_t)), "produce", T_produce, (void*)(uintptr_t)i);
         kmt->create(pmm->alloc(sizeof(task_t)), "consume", T_consume, (void*)(uintptr_t)i);
     }

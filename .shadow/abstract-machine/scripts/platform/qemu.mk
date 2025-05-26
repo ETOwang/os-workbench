cc00a8ab7bef7ed6e9213e73f8bc6f27e0f042e3
tracer-nju <tracer@nju.edu.cn>
2025-05-26 14:05:03 +0800

    Linux wniux 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:05:03 up 5 min,  1 user,  load average: 1.07, 0.90, 0.42

--- .shadow/abstract-machine/scripts/platform/qemu.mk
+++ .shadow/abstract-machine/scripts/platform/qemu.mk
@@ -1,20 +1,12 @@
 .PHONY: build-arg
 
 smp        ?= 8
-TESTDISK   ?= /media/wcy/Data/os-workbench/kernel/sdcard-rv.img
 LDFLAGS    += -N -Ttext-segment=0x00100000
 QEMU_FLAGS += -serial mon:stdio \
               -machine accel=tcg \
               -smp "cores=1,sockets=$(smp)" \
               -drive format=raw,file=$(IMAGE) \
-              -drive file=$(TESTDISK),if=none,format=raw,id=x0 \
-              -device virtio-blk-pci,drive=x0
-
-# 如果存在 disk.img，则添加第二个磁盘
-ifneq ($(wildcard $(BOOTDISK)),)
-QEMU_FLAGS += -drive file=$(BOOTDISK),if=none,format=raw,id=x1 \
-              -device virtio-blk-pci,drive=x1
-endif
+			  -drive format=raw,file=/media/wcy/Data/os-workbench/kernel/sdcard-rv.img
 
 build-arg: image
 	@( echo -n $(mainargs); ) | dd if=/dev/stdin of=$(IMAGE) bs=512 count=2 seek=1 conv=notrunc status=none

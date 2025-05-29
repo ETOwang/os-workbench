a4e4408872dd468da16a3a522bda8390d186948b
tracer-nju <tracer@nju.edu.cn>
2025-05-29 13:08:52 +0800

    Linux wniux 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:08:52 up  4:03,  1 user,  load average: 1.47, 1.52, 1.26

--- .shadow/abstract-machine/scripts/x86_64-qemu.mk
+++ .shadow/abstract-machine/scripts/x86_64-qemu.mk
@@ -9,8 +9,7 @@ AM_SRCS := x86/qemu/start64.S \
            x86/qemu/vme.c \
            x86/qemu/mpe.c
 
-run: build-arg
-	@qemu-system-x86_64 $(QEMU_FLAGS)
-
 kernel:build-arg
     @qemu-system-x86_64 $(QEMU_KERNEL_FLAGS)
+run:build-arg
+	@qemu-system-x86_64 $(QEMU_FLAGS)

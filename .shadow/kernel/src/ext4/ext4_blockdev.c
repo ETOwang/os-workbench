a8879629af21917f9d939868e54a43e0fefe0b35
tracer-nju <tracer@nju.edu.cn>
2025-05-29 17:09:16 +0800

    Linux wniux 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:09:16 up  8:03,  1 user,  load average: 2.72, 2.69, 2.29

--- .shadow/kernel/src/ext4/ext4_blockdev.c
+++ .shadow/kernel/src/ext4/ext4_blockdev.c
@@ -387,7 +387,7 @@ int ext4_block_readbytes(struct ext4_blockdev *bdev, uint64_t off, void *buf,
 	uint8_t *p = (void *)buf;
 
 	ext4_assert(bdev && buf);
- 
+    printf("readbytes\n");
 	if (!bdev->bdif->ph_refctr)
 		return EIO;
 	if (off + len > bdev->part_size)

2486af6f6d4747805abbca2fbb20b59f03de258c
tracer-nju <tracer@nju.edu.cn>
2025-06-03 10:43:48 +0800

    Linux wniux 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:43:48 up  1:30,  1 user,  load average: 3.28, 3.11, 2.95

--- .shadow/kernel/src/syscall.c
+++ .shadow/kernel/src/syscall.c
@@ -93,10 +93,10 @@ static uint64_t syscall_openat(task_t *task, int fd, const char *filename, int f
         return -1;
     }
     // TODO:use mode
-    printf("openat %s\n", full_path);
     int vfd = vfs->open(full_path, flags);
     if (vfd < 0)
     {
+        printf("openat failed: %s\n", full_path);
         return -1;
     }
     return vfd;

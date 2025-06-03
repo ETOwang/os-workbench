059b629aa5d52c157c3c6fdaddd5312841b44b4f
tracer-nju <tracer@nju.edu.cn>
2025-06-03 09:22:29 +0800

    Linux wniux 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:22:29 up 8 min,  1 user,  load average: 1.68, 1.61, 0.84

--- .shadow/kernel/src/uproc.c
+++ .shadow/kernel/src/uproc.c
@@ -55,7 +55,6 @@ static int uproc_exit(task_t *task, int status)
 {
     panic_on(task == NULL, "Task is NULL");
     panic_on(task->pi == NULL, "Task procinfo is NULL");
-    printf("uproc_exit called with status=%d\n", status);
     task->pi->xstate = status;
     task->status = TASK_ZOMBIE;
     return 0;
@@ -79,7 +78,7 @@ int uvmcopy(AddrSpace *old, AddrSpace *new, uint64_t sz)
             break;
         }
         uintptr_t *ptep = ptewalk(old, current_va);
-        if (ptep && (*ptep & PROT_NONE))
+        if (ptep && (*ptep & PROT_READ))
         {
             int map_prot = MMAP_READ;
             if (*ptep & MMAP_WRITE)

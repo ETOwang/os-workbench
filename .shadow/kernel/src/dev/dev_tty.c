a9c68be8b41e3511ac6c8d98b9e1c8db9e87f212
tracer-nju <tracer@nju.edu.cn>
2025-06-11 12:21:27 +0800

    Linux wniux 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:21:27 up  4:06,  1 user,  load average: 1.73, 2.93, 3.27

--- .shadow/kernel/src/dev/dev_tty.c
+++ .shadow/kernel/src/dev/dev_tty.c
@@ -247,8 +247,8 @@ static int tty_read(device_t *dev, size_t offset, void *buf, int count) {
 static int tty_write(device_t *dev, size_t offset, const void *buf, int count) {
   tty_t *tty = dev->ptr;
   kmt->spin_lock(&tty->lock);
-  printf("buf:%s\n", (const char *)buf);
   for (int i = 0; i < count; i++) {
+    printf("tty_write: %c\n", ((const char *)buf)[i]);
     tty_putc(tty, ((const char *)buf)[i]);
   }
   kmt->spin_unlock(&tty->lock);

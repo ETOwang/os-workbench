a8835b7e132a25386db8c0161a659e9d489a4fec
tracer-nju <tracer@nju.edu.cn>
2025-06-11 15:42:04 +0800

    Linux wniux 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:42:04 up  7:27,  1 user,  load average: 3.19, 3.91, 3.00

--- .shadow/kernel/src/vfs.c
+++ .shadow/kernel/src/vfs.c
@@ -159,7 +159,7 @@ ssize_t vfs_read(int fd, void *buf, size_t count)
 		tty_t *tty1 = tty->ptr;
 		printf("VFS: Read from tty1\n");
 		while(tty1->cooked.value==0){
-             printf("%d\n",tty1->cooked.value);
+            
 		}
 		printf("VFS: Read from tty1\n");
 		return tty->ops->read(tty,0,buf,count);

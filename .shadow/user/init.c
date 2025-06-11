a130ff5c58292a9a3e3c3c1e20a8534d6ad1f54a
tracer-nju <tracer@nju.edu.cn>
2025-06-11 22:14:50 +0800

    Linux wniux 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:14:50 up 58 min,  1 user,  load average: 1.32, 1.74, 2.05

--- .shadow/user/init.c
+++ .shadow/user/init.c
@@ -155,7 +155,7 @@ int getcmd(char *buf, int nbuf)
 }
 void main()
 {
-    static char buf[100];
+    static char buf[10];
 
     // Read and run input commands.
     while (getcmd(buf, sizeof(buf)) >= 0)

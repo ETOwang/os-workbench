dc7761d46cef33976c0eb76bbc6ec6396687de08
tracer-nju <tracer@nju.edu.cn>
2025-06-11 08:30:49 +0800

    Linux wniux 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:30:49 up 16 min,  1 user,  load average: 0.67, 0.57, 0.36

--- .shadow/user/init.c
+++ .shadow/user/init.c
@@ -279,77 +279,8 @@ void simple_shell()
 
 int main()
 {
-    print("BusyBox Init Process Starting...\n");
-
-    // 文件系统已经在VFS初始化时自动挂载了，不需要重新挂载
-    print("Root filesystem already mounted by kernel\n");
-
-    // 切换到根目录
-    if (my_chdir("/") < 0)
-    {
-        print("Warning: Failed to change to root directory\n");
-    }
-
-    // 检查BusyBox是否存在
-    print("Looking for BusyBox binary...\n");
-
-    // 尝试打开 /bin/busybox
-    print("Trying /bin/busybox...\n");
-    int busybox_fd = my_openat(AT_FDCWD, "/bin/busybox", O_RDONLY);
-    if (busybox_fd < 0)
-    {
-        print("Failed to open /bin/busybox, trying /busybox...\n");
-        busybox_fd = my_openat(AT_FDCWD, "/busybox", O_RDONLY);
-        if (busybox_fd < 0)
-        {
-            print("Failed to open /busybox too\n");
-        }
-        else
-        {
-            print("Found /busybox!\n");
-        }
-    }
-    else
-    {
-        print("Found /bin/busybox!\n");
-    }
-
-    if (busybox_fd < 0)
-    {
-        print("Error: BusyBox binary not found!\n");
-        print("Starting simple shell instead...\n\n");
-
-        // 启动简单shell
-        simple_shell();
-    }
-    else
-    {
-        my_close(busybox_fd);
-        print("BusyBox found! Executing...\n");
-
-        // 准备参数
-        char *argv[] = {"busybox", "sh", (char *)0};
-        char *envp[] = {"PATH=/bin:/usr/bin", "HOME=/", (char *)0};
-
-        // 尝试执行BusyBox shell
-        int exec_result = my_execve("/bin/busybox", argv, envp);
-        if (exec_result < 0)
-        {
-            exec_result = my_execve("/busybox", argv, envp);
-        }
-
-        if (exec_result < 0)
-        {
-            print("Error: Failed to execute BusyBox!\n");
-            print("Error code: ");
-            print_int(exec_result);
-            print("\n");
-        }
-
-        // 如果execve失败，进入简单shell
-        print("Falling back to simple shell...\n\n");
-        simple_shell();
-    }
+    simple_shell();
+    
 
     return 0;
 }

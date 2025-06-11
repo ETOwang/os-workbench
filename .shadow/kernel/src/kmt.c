beca801c67a62483e9699cd63b9cfbfb24538a1b
tracer-nju <tracer@nju.edu.cn>
2025-06-11 22:06:09 +0800

    Linux wniux 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:06:09 up 49 min,  1 user,  load average: 3.86, 3.26, 2.58

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -197,6 +197,7 @@ static Context *kmt_pgfault(Event ev, Context *ctx)
     printf("Page fault at %p\n", ev.ref);
     printf("Page fault msg: %s\n", ev.msg);
     printf("Page fault cause: %p\n", ev.cause);
+    printf("data in ref:%s\n",ev.ref);
     halt(1);
     return NULL;
 }

db0e09109c8d564fc4825deca17f044c89d76c14
tracer-nju <tracer@nju.edu.cn>
2025-06-11 21:36:53 +0800

    Linux wniux 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:36:53 up 20 min,  1 user,  load average: 0.96, 1.66, 1.59

--- .shadow/user/myulib.h
+++ .shadow/user/myulib.h
@@ -46,24 +46,26 @@
 #define O_RDWR 2
 
 // 简单的时间结构
-struct my_timespec {
-    long tv_sec;
-    long tv_nsec;
+struct my_timespec
+{
+  long tv_sec;
+  long tv_nsec;
 };
 
-static inline long syscall(int num, ...) {
-    va_list ap;
-    va_start(ap, num);
-    register long a0 asm("rax") = num;
-    register long a1 asm("rdi") = va_arg(ap, long);
-    register long a2 asm("rsi") = va_arg(ap, long);
-    register long a3 asm("rdx") = va_arg(ap, long);
-    register long a4 asm("r10") = va_arg(ap, long);
-    va_end(ap);
-    asm volatile("int $0x80"
-                 : "+r"(a0) : "r"(a1), "r"(a2), "r"(a3), "r"(a4)
-                 : "memory", "rcx", "r8", "r9", "r11");
-    return a0;
+static inline long syscall(int num, ...)
+{
+  va_list ap;
+  va_start(ap, num);
+  register long a0 asm("rax") = num;
+  register long a1 asm("rdi") = va_arg(ap, long);
+  register long a2 asm("rsi") = va_arg(ap, long);
+  register long a3 asm("rdx") = va_arg(ap, long);
+  register long a4 asm("r10") = va_arg(ap, long);
+  va_end(ap);
+  asm volatile("int $0x80"
+               : "+r"(a0) : "r"(a1), "r"(a2), "r"(a3), "r"(a4)
+               : "memory", "rcx", "r8", "r9", "r11");
+  return a0;
 }
 
 static inline int my_kputc(char ch)
@@ -126,58 +128,100 @@ static inline int my_execve(const char *filename, char *const argv[], char *cons
   return syscall(SYS_execve, (uint64_t)filename, (uint64_t)argv, (uint64_t)envp, 0);
 }
 
-
-static inline size_t strlen(const char *s) {
-    size_t len = 0;
-    for (; *s; s++)
-        len++;
-    return len;
+static inline size_t strlen(const char *s)
+{
+  size_t len = 0;
+  for (; *s; s++)
+    len++;
+  return len;
 }
 
-static inline char *strcpy(char *d, const char *s) {
-    char *r = d;
-    while (*s) {
-        *d++ = *s++;
+static inline char *strcpy(char *d, const char *s)
+{
+  char *r = d;
+  while (*s)
+  {
+    *d++ = *s++;
+  }
+  *d = '\0';
+  return r;
+}
+
+static inline char *strchr(const char *s, int c)
+{
+  for (; *s; s++)
+  {
+    if (*s == c)
+      return (char *)s;
+  }
+  return NULL;
+}
+
+static inline void print(const char *s, ...)
+{
+  va_list ap;
+  va_start(ap, s);
+  while (s != NULL)
+  {
+    syscall(SYS_write, 2, s, strlen(s));
+    s = va_arg(ap, const char *);
+  }
+  va_end(ap);
+}
+
+static inline void print_int(int num)
+{
+  char buf[32];
+  char *p = buf + sizeof(buf) - 1;
+  *p = '\0';
+
+  int is_negative = 0;
+  if (num < 0)
+  {
+    is_negative = 1;
+    num = -num;
+  }
+
+  if (num == 0)
+  {
+    *(--p) = '0';
+  }
+  else
+  {
+    while (num > 0)
+    {
+      *(--p) = '0' + (num % 10);
+      num /= 10;
     }
-    *d = '\0';
-    return r;
+  }
+
+  if (is_negative)
+  {
+    *(--p) = '-';
+  }
+
+  syscall(SYS_write, 2, p, strlen(p));
 }
 
-static inline char *strchr(const char *s, int c) {
-    for (; *s; s++) {
-        if (*s == c)
-            return (char *)s;
-    }
-    return NULL;
-}
-
-static inline void print(const char *s, ...) {
-    va_list ap;
-    va_start(ap, s);
-    while (s!=NULL) {
-        syscall(SYS_write, 2, s, strlen(s));
-        s = va_arg(ap, const char *);
-    }
-    va_end(ap);
-}
-
-#define assert(cond)                    \
-    do {                                \
-        if (!(cond))                    \
-        {                               \
-            print("Panicked.\n", NULL); \
-            syscall(SYS_exit, 1);       \
-        }                               \
-    } while (0)
+#define assert(cond)              \
+  do                              \
+  {                               \
+    if (!(cond))                  \
+    {                             \
+      print("Panicked.\n", NULL); \
+      syscall(SYS_exit, 1);       \
+    }                             \
+  } while (0)
 
 static char mem[4096], *freem = mem;
 
-static inline void *zalloc(size_t sz) {
-    // This is memory leak!
-    assert(freem + sz < mem + sizeof(mem));
-    void *ret = freem;
-    freem += sz;
-    return ret;
+static inline void *zalloc(size_t sz)
+{
+  // This is memory leak!
+  assert(freem + sz < mem + sizeof(mem));
+  void *ret = freem;
+  freem += sz;
+  return ret;
 }
 
 #endif // __MYULIB_H__

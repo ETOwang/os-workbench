ba5f3154de421a43253337cd050e902f2a86ae0f
tracer-nju <tracer@nju.edu.cn>
2025-06-12 10:51:04 +0800

    Linux wniux 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:51:04 up  2:29,  1 user,  load average: 1.38, 1.71, 2.02

--- .shadow/kernel/src/dev/dev_tty.c
+++ .shadow/kernel/src/dev/dev_tty.c
@@ -266,7 +266,7 @@ static int tty_init(device_t *ttydev)
   struct tty_queue *q = &tty->queue;
   q->front = q->rear = q->buf = pmm->alloc(TTY_COOK_BUF_SZ);
   q->end = q->buf + TTY_COOK_BUF_SZ;
-  kmt->spin_init(&tty->lock, "tty lock");
+  kmt->spin_init(&tty->lock, ttydev->name);
   kmt->sem_init(&tty->cooked, "tty cooked lines", 0);
   welcome(ttydev);
   return 0;

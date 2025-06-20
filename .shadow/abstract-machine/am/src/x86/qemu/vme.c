405cc8435499fb736ffda620e8d57d611c5e81d6
tracer-nju <tracer@nju.edu.cn>
2025-06-20 20:11:39 +0800

    Linux wniux 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:11:39 up 27 min,  1 user,  load average: 0.36, 0.78, 0.90

--- .shadow/abstract-machine/am/src/x86/qemu/vme.c
+++ .shadow/abstract-machine/am/src/x86/qemu/vme.c
@@ -118,18 +118,18 @@ bool vme_init(void *(*_pgalloc)(int size), void (*_pgfree)(void *)) {
 void protect(AddrSpace *as) {
   uintptr_t *upt = pgallocz();
 
-  // for (int i = 0; i < LENGTH(vm_areas); i++) {
-  //   const struct vm_area *vma = &vm_areas[i];
-  //   if (vma->kernel) {
-  //     const struct ptinfo *info = &mmu.pgtables[1]; // level-1 page table
-  //     for (uintptr_t cur = (uintptr_t)vma->area.start;
-  //          cur != (uintptr_t)vma->area.end;
-  //          cur += (1L << info->shift)) {
-  //       int index = indexof(cur, info);
-  //       upt[index] = kpt[index];
-  //     }
-  //   }
-  // }
+  for (int i = 0; i < LENGTH(vm_areas); i++) {
+    const struct vm_area *vma = &vm_areas[i];
+    if (vma->kernel) {
+      const struct ptinfo *info = &mmu.pgtables[1]; // level-1 page table
+      for (uintptr_t cur = (uintptr_t)vma->area.start;
+           cur != (uintptr_t)vma->area.end;
+           cur += (1L << info->shift)) {
+        int index = indexof(cur, info);
+        upt[index] = kpt[index];
+      }
+    }
+  }
   as->pgsize = mmu.pgsize;
   as->area   = uvm_area;
   as->ptr    = (void *)((uintptr_t)upt | PTE_P | PTE_U);

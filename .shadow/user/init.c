ad9324ae789cb1b646900b6b87d6d30bc887f16d
tracer-nju <tracer@nju.edu.cn>
2025-06-21 17:48:14 +0800

    Linux wniux 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:48:14 up  8:14,  1 user,  load average: 0.66, 0.88, 0.79

--- .shadow/user/init.c
+++ .shadow/user/init.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
     // Simple init that just execs sh
     char *sh_argv[] = {"/bin/sh", NULL};
 
+    execve("/bin/sh", sh_argv, NULL);
     // Use execve to replace this process with sh
     syscall(SYS_execve, "/bin/sh", sh_argv, NULL);
     // If execve fails, try sh without path
